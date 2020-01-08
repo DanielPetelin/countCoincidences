32283f9b97f4e72e3fd88463011ab80df8b7488b
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-08 10:14:06 +0300

    countCoincidences

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-var a = ["п","р","ы","в","е","т"],
-    b = ["п","р"];
+var h = ["п", "р", "ы", "в", "е", "т", 12, 546, 85];
+var c = ["п", "fsa", 123, 85];
+var g = [2123, 54, 23, 65, 23, 76, 12];
 
 var yes;
 
-var ln1 = a.length
-    ln2 = b.length;
+function countCoincidences(a, b) {
+    for (var i = 0; i < a.length; i++) {
+    yes = a[i];
+        for (var j = 0; j < b.length; j++) {
+            if (yes == b[j]) {
+            console.log('найдено совпадение: ' + yes);
+            break;
+            } else {
+                console.log('совпадений не обнаружено');
+            };
+        };
+    };
+};
 
-for (var i = 0; i < ln1; i++) {
-yes = a[i];
-    for (var j = 0; j < ln2; j++) {
-    if (yes == b[j]) {
-    console.log('найдено совпадение: ' + yes);
-    break;
-        }
-    }
-}
+console.log(countCoincidences(c, h));
